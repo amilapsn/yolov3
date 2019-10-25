fbad2d40248a0c1fb0691f806089c6a05f5ab756
amila <amila.ravinath@axiatadigitallabs.com>
2019-10-25 11:02:46 +0530

    DEFAULT BATCH SIZE TO 8, TEST LOSS NORMALIZE

--- test.py
+++ test.py
@@ -171,12 +171,12 @@ def test(
         map = cocoEval.stats[1]  # update mAP to pycocotools mAP
 
     # Return results
-    return mp, mr, map, mf1, loss
+    return mp, mr, map, mf1, loss/len(dataloader)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='test.py')
-    parser.add_argument('--batch-size', type=int, default=32, help='size of each image batch')
+    parser.add_argument('--batch-size', type=int, default=8, help='size of each image batch')
     parser.add_argument('--cfg', type=str, default='cfg/yolov3-spp.cfg', help='cfg file path')
     parser.add_argument('--data-cfg', type=str, default='data/coco.data', help='coco.data file path')
     parser.add_argument('--weights', type=str, default='weights/yolov3-spp.weights', help='path to weights file')
